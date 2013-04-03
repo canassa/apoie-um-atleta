17b2966b2a66ba71027830bdb6e31a6fe6efac46
Cesar Canassa <cesar.canassa@gmail.com>
2013-04-02 23:47:18 -0300

    Added fonts

--- src/atleta/css.clj
+++ src/atleta/css.clj
@@ -1,15 +1,44 @@
 (ns atleta.css
   (:require (gaka [core :as gaka])))
 
+
 (def half-screen (list :float "left" :width "50%"))
 
-(defn url [img] (str "url(/img/" img ")"))
+
+(defn url [& args]
+  (str "url(/" (apply str args) ")"))
+
+(def url-img (partial url "img/"))
+(def url-font (partial url "font/"))
+
+
+(defn font-face [family file weight] [
+  "@font-face"
+    :font-family family
+    :src (url-font file ".eot")
+    :src (clojure.string/join ", " [
+           (str (url-font file ".eot?#iefix") " format('embedded-opentype')")
+           (str (url-font file ".woff") " format('woff')")
+           (str (url-font file ".ttf") " format('truetype')")
+           (str (url-font file ".svg#" file) " format('svg')")])
+    :font-weight weight
+    :font-style "normal"
+    ])
+
 
 (def rule [
+  (font-face "caturrita" "caturrita-medium" "bold")
+  (font-face "caturrita" "caturrita-regular" "normal")
            
+  [:body
+    :font-family "\"caturrita\", serif"]
+           
+  ["h1, h2, h3"
+    :font-weight "bold"]
+
   ["body > header"
     :height "270px"
-    :background (url "bg-header.png")
+    :background (url-img "bg-header.png")
     [:h1
       :color "white"
       :text-align "center"
@@ -18,7 +47,7 @@
       :width "600px"
       :height "50px"
       :margin "0 auto"
-      :background (url "bg-menu.png")
+      :background (url-img "bg-menu.png")
       [:a
         :color "white"
         :text-decoration "none"]
@@ -40,7 +69,7 @@
            
   [:footer
     :height "35px"
-    :background (url "bg-header.png")]
+    :background (url-img "bg-header.png")]
   ])
 
 (defn -main []
